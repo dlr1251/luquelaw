31accd3ede4213110ffbcaa250ccdc694bd214b6
Daniel <daniel.luqx@gmail.com>
2022-12-13 11:23:04 -0500

    Add forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [
   // Enable Preact to support Preact JSX components.
   preact(),
