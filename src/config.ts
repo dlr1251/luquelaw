add5c0aebd70cce2c7b01c94125ca3406063a98b
Daniel <daniel.luqx@gmail.com>
2022-12-28 09:35:24 -0500

    Add SEO image

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
+		src: 'https://images.unsplash.com/photo-1463680942456-e4230dbeaec7?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=6000',
 		alt:
 			'astro logo on a starry expanse of space,' +
 			' with a purple saturn-like planet floating in the right foreground',
